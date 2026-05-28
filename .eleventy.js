f3de7f10dd30b093de3266da1fb656a16e6fe069
Michael Polom <michaelpolom@gmail.com>
2026-05-28 18:32:57 +0100

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(require("./src/_11ty/cssmini.js")); // CSS minification
+  eleventyConfig.addPlugin(require("./src/_11ty/datetime.js")); // Date Time plugin
+  eleventyConfig.addPlugin(require("./src/_11ty/passthroughs.js")); // passthroughs
+
+  eleventyConfig.addPassthroughCopy("admin");
+  
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};    
