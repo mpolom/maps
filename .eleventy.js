cc1c917f033767c978116b22e3eb507b3bbb84e6
Michael Polom <michaelpolom@gmail.com>
2026-05-30 09:58:37 +0100

    Add sass functionality

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,11 @@
+const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(require("./src/_11ty/cssmini.js")); // CSS minification
   eleventyConfig.addPlugin(require("./src/_11ty/datetime.js")); // Date Time plugin
   eleventyConfig.addPlugin(require("./src/_11ty/passthroughs.js")); // passthroughs
 
+  eleventyConfig.addPlugin(eleventySass);
   
   return {
     dir: {
